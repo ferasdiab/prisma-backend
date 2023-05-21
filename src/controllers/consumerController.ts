68081d8f426f4faf571185371f8ee41a42cc7b0e
ferasdiab <dfiras96@gmail.com>
2023-05-21 10:00:09 +0300

    refactor the  consumer sevices

--- src/controllers/consumerController.ts
+++ src/controllers/consumerController.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from "express";
 import prisma from "../../script";
 import { Prisma } from "@prisma/client";
+import {
+  createConsumerService,
+  getConsumersService,
+} from "../services/consumer";
 
 export const createConsumer = async (req: Request, res: Response) => {
   try {
     // Your logic to fetch users from the database using Prisma
     const { name } = req.body;
-    const user = await prisma.consumer.create({
-      data: {
-        name,
-      },
-    });
+    const user = await createConsumerService(name);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: error });
@@ -21,32 +21,8 @@ export const getConsumers = async (req: Request, res: Response) => {
   try {
     // Your logic to fetch users from the database using Prisma
     const { name } = req.body;
-    let whereCondition: Prisma.ConsumerWhereInput = { isDeleted: false };
-    if (name) {
-      whereCondition = {
-        ...whereCondition,
-        name: {
-          path: "$.en",
-          string_contains: name,
-        },
-      };
-    }
-    const users = await prisma.consumer.findMany({
-      where: whereCondition,
-      include: {
-        Appointments: true,
-        ConsumerBasket: {
-          include: {
-            Package: true,
-            ConsumerBasketService: {
-              include: {
-                Service: true,
-              },
-            },
-          },
-        },
-      },
-    });
+    const users = await getConsumersService(name);
+
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: error });
---
+++ src/services/consumer.ts
@@ -0,0 +1,47 @@
+import prisma from "../../script";
+import { Prisma } from "@prisma/client";
+
+export const createConsumerService = async (name: string) => {
+  try {
+    return await prisma.consumer.create({
+      data: {
+        name,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getConsumersService = async (name: string) => {
+  let whereCondition: Prisma.ConsumerWhereInput = { isDeleted: false };
+  if (name) {
+    whereCondition = {
+      ...whereCondition,
+      name: {
+        path: "$.en",
+        string_contains: name,
+        // mode: "insensitive",
+      },
+    };
+  }
+
+  const users = await prisma.consumer.findMany({
+    where: whereCondition,
+    include: {
+      Appointments: true,
+      ConsumerBasket: {
+        include: {
+          Package: true,
+          ConsumerBasketService: {
+            include: {
+              Service: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return users;
+};
