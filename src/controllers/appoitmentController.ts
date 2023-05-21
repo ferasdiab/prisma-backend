585f4e05b533930ed66b1d2be9bbdba84eebe619
ferasdiab <dfiras96@gmail.com>
2023-05-21 11:03:02 +0300

    Remove  dead  code

--- src/controllers/appoitmentController.ts
+++ src/controllers/appoitmentController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import prisma from "../../script";
-import { Prisma } from "@prisma/client";
 import {
   createAppointmentBusiness,
   getAppointmentsBusiness,
